8ac96f5f518886a1dd1d417f10eb5d0f808766d1
sh-sergey <shchelkanov-s@yandex.ru>
2021-05-15 14:03:48 +0300

    update gpio int example

--- examples/gpio_int.c
+++ examples/gpio_int.c
@@ -4,23 +4,30 @@
 
 /**
  * \file Пример работы с прерываниями портов ввода-вывода
+ * \note Пример создавался для работы с моделью МК на ПЛИС, и зависит от
+ * переключателей на плате. Переключатели на GPIO_2 с вывода 7 по 4
+ * должны быть установлены следующим образом: vvv^. В примере переключатели
+ * на выводах 7 и 6 упрявляют светодиодами 3 и 2 соответственно, переключатель
+ * 5 в верхнем положении включает светодиод 1, переключатель 4
+ * в нижнем положении выключает светодиод 1. Светодиод 0
+ * переключается в главном цикле, и показывает, исполняется ли главный цикл.
  */
 
 void GPIO_IRQ_TRAP_HANDLER() {
 	if (GPIO_LineInterruptState(7)) {
-		GPIO_PinWrite(GPIO_2, 0, GPIO_LinePinState(7));
+		GPIO_PinWrite(GPIO_2, 3, GPIO_LinePinState(7));
 	}
 	if (GPIO_LineInterruptState(6)) {
-		GPIO_PinWrite(GPIO_2, 1, GPIO_HIGH);
-	}
-	if (GPIO_LineInterruptState(2)) {
-		GPIO_PinWrite(GPIO_2, 1, GPIO_LOW);
-	}
-	if (GPIO_LineInterruptState(5)) {
 		GPIO_PinWrite(GPIO_2, 2, GPIO_HIGH);
 	}
-	if (GPIO_LineInterruptState(4)) {
+	if (GPIO_LineInterruptState(2)) {
 		GPIO_PinWrite(GPIO_2, 2, GPIO_LOW);
+	}
+	if (GPIO_LineInterruptState(5)) {
+		GPIO_PinWrite(GPIO_2, 1, GPIO_HIGH);
+	}
+	if (GPIO_LineInterruptState(4)) {
+		GPIO_PinWrite(GPIO_2, 1, GPIO_LOW);
 	}
 	GPIO_ClearInterrupt();
 }
@@ -31,22 +38,20 @@ int main()
 
 	GPIO_SetPinMaskDirection(GPIO_2, PIN_0 | PIN_1 | PIN_2 | PIN_3, GPIO_DIR_OUTPUT);
 
-	GPIO_PinWrite(GPIO_2, 0, GPIO_LOW);
-	GPIO_PinWrite(GPIO_2, 1, GPIO_LOW);
-	GPIO_PinWrite(GPIO_2, 2, GPIO_LOW);
+	GPIO_PIN_MASK_CLEAR(GPIO_2, 0b1111);
 
 	GPIO_SetPinMaskDirection(GPIO_2, PIN_7 | PIN_6 | PIN_5 | PIN_4, GPIO_DIR_INPUT);
 
-	GPIO_InitInterruptLine(GPIO_LINE_7, GPIO_MUX_LINE_7_GPIO_2_7, GPIO_MODE_CHANGE);
-	GPIO_InitInterruptLine(GPIO_LINE_6, GPIO_MUX_LINE_6_GPIO_2_6, GPIO_MODE_RISING);
-	GPIO_InitInterruptLine(GPIO_LINE_2, GPIO_MUX_LINE_2_GPIO_2_6, GPIO_MODE_FALLING);
-	GPIO_InitInterruptLine(GPIO_LINE_5, GPIO_MUX_LINE_5_GPIO_2_5, GPIO_MODE_HIGH);
-	GPIO_InitInterruptLine(GPIO_LINE_4, GPIO_MUX_LINE_4_GPIO_2_4, GPIO_MODE_LOW);
+	GPIO_InitInterruptLine(GPIO_LINE_7, GPIO_MUX_LINE_7_GPIO_2_7, GPIO_MODE_INT_CHANGE);
+	GPIO_InitInterruptLine(GPIO_LINE_6, GPIO_MUX_LINE_6_GPIO_2_6, GPIO_MODE_INT_RISING);
+	GPIO_InitInterruptLine(GPIO_LINE_2, GPIO_MUX_LINE_2_GPIO_2_6, GPIO_MODE_INT_FALLING);
+	GPIO_InitInterruptLine(GPIO_LINE_5, GPIO_MUX_LINE_5_GPIO_2_5, GPIO_MODE_INT_HIGH);
+	GPIO_InitInterruptLine(GPIO_LINE_4, GPIO_MUX_LINE_4_GPIO_2_4, GPIO_MODE_INT_LOW);
 
 	enableInterrupts();
 
 	while (1) {
-		delay(1000);
-		GPIO_PinToggle(GPIO_2, 3);
+		delay(500);
+		GPIO_PinToggle(GPIO_2, 0);
 	}
 }
