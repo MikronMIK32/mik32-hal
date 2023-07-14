382e18cb3c883b551fef6a0d6aac8d0c524e1ff3
Sergey Shchelkanov <shchelkanov-s@yandex.ru>
2023-07-14 17:06:57 +0300

    very early spifi

---
+++ peripherals/Include/mik32_hal_spifi.h
@@ -0,0 +1,93 @@
+#ifndef MIK32_HAL_SPIFI
+#define MIK32_HAL_SPIFI
+
+#include <mcu32_memory_map.h>
+#include <spifi.h>
+
+typedef enum
+{
+    SPIFI_CACHE_DISABLE = 0,
+    SPIFI_CACHE_ENABLE = 1
+} HAL_SPIFI_CacheEnableTypeDef;
+
+typedef enum
+{
+    SPIFI_FIELDFORM_ALL_SERIAL = 0,
+    SPIFI_FIELDFORM_DATA_PARALLEL = 1,
+    SPIFI_FIELDFORM_COMMAND_SERIAL = 1,
+    SPIFI_FIELDFORM_ALL_PARALLEL = 1
+} HAL_SPIFI_FieldFormTypeDef;
+
+typedef enum
+{
+    SPIFI_FRAMEFORM_OPCODE = 1,
+    SPIFI_FRAMEFORM_OPCODE_1ADDR = 2,
+    SPIFI_FRAMEFORM_OPCODE_2ADDR = 3,
+    SPIFI_FRAMEFORM_OPCODE_3ADDR = 4,
+    SPIFI_FRAMEFORM_OPCODE_4ADDR = 5,
+    SPIFI_FRAMEFORM_3ADDR = 6,
+    SPIFI_FRAMEFORM_4ADDR = 7
+} HAL_SPIFI_FrameFormTypeDef;
+
+typedef enum
+{
+    SPIFI_DIRECTION_INPUT = 0,
+    SPIFI_DIRECTION_OUTPUT = 1
+} HAL_SPIFI_DirectionTypeDef;
+
+typedef struct
+{
+    uint8_t InterimLength;
+
+    HAL_SPIFI_FieldFormTypeDef FieldForm;
+
+    HAL_SPIFI_FrameFormTypeDef FrameForm;
+
+    uint8_t OpCode;
+
+} SPIFI_MemoryCommandTypeDef;
+
+typedef struct
+{
+
+    SPIFI_CONFIG_TypeDef *Instance;
+
+    HAL_SPIFI_CacheEnableTypeDef CacheEnable;
+
+    uint32_t CacheLimit;
+
+    SPIFI_MemoryCommandTypeDef Command;
+
+} SPIFI_MemoryModeConfig_HandleTypeDef;
+
+typedef struct
+{
+
+    SPIFI_CONFIG_TypeDef *Instance;
+
+} SPIFI_HandleTypeDef;
+
+typedef struct
+{
+    HAL_SPIFI_DirectionTypeDef Direction;
+
+    uint8_t InterimLength;
+
+    HAL_SPIFI_FieldFormTypeDef FieldForm;
+
+    HAL_SPIFI_FrameFormTypeDef FrameForm;
+
+    uint8_t OpCode;
+
+} SPIFI_CommandTypeDef;
+
+void HAL_SPIFI_MemoryMode_Init(SPIFI_MemoryModeConfig_HandleTypeDef *spifi);
+
+void HAL_SPIFI_SendCommand(
+    SPIFI_HandleTypeDef *spifi,
+    SPIFI_CommandTypeDef cmd,
+    uint32_t address,
+    uint16_t dataLength,
+    uint8_t *dataBytes);
+
+#endif // MIK32_HAL_SPIFI
---
+++ peripherals/Source/mik32_hal_spifi.c
@@ -0,0 +1,61 @@
+#include <mik32_hal_spifi.h>
+
+void HAL_SPIFI_MemoryMode_Init(SPIFI_MemoryModeConfig_HandleTypeDef *spifi)
+{
+    spifi->Instance->STAT |= SPIFI_CONFIG_STAT_RESET_M;
+    spifi->Instance->CLIMIT = spifi->CacheLimit; // Граница кеширования
+    if (spifi->CacheEnable)
+    {
+        spifi->Instance->CTRL |= SPIFI_CONFIG_CTRL_CACHE_EN_M;
+    }
+    else
+    {
+        spifi->Instance->CTRL &= ~SPIFI_CONFIG_CTRL_CACHE_EN_M;
+    }
+
+    // Настройка команды чтения
+    spifi->Instance->MCMD = ((spifi->Command.InterimLength << SPIFI_CONFIG_MCMD_INTLEN_S) |
+                             (spifi->Command.FieldForm << SPIFI_CONFIG_MCMD_FIELDFORM_S) |
+                             (spifi->Command.FrameForm << SPIFI_CONFIG_MCMD_FRAMEFORM_S) |
+                             (spifi->Command.OpCode << SPIFI_CONFIG_MCMD_OPCODE_S));
+}
+
+
+void SPIFI_WaitIntrqTimeout()
+{
+    while (~(SPIFI_CONFIG->STAT & SPIFI_CONFIG_STAT_INTRQ_M));
+}
+
+void HAL_SPIFI_SendCommand(
+    SPIFI_HandleTypeDef *spifi,
+    SPIFI_CommandTypeDef cmd,
+    uint32_t address,
+    uint16_t dataLength,
+    uint8_t *dataBytes)
+{
+    spifi->Instance->STAT |= SPIFI_CONFIG_STAT_INTRQ_M;
+    spifi->Instance->ADDR = address;
+    spifi->Instance->CMD = ((cmd.OpCode << SPIFI_CONFIG_CMD_OPCODE_S) |
+                            (cmd.FrameForm << SPIFI_CONFIG_CMD_FRAMEFORM_S) |
+                            (cmd.FieldForm << SPIFI_CONFIG_CMD_FIELDFORM_S) |
+                            (dataLength << SPIFI_CONFIG_CMD_DATALEN_S) |
+                            (cmd.InterimLength << SPIFI_CONFIG_CMD_INTLEN_S) |
+                            (cmd.Direction << SPIFI_CONFIG_CMD_DOUT_S));
+    
+    // SPIFI_WaitIntrqTimeout();
+
+    if (cmd.Direction == SPIFI_DIRECTION_INPUT)
+    {
+        for (int i = 0; i < dataLength; i++)
+        {
+            dataBytes[i] = (uint8_t)spifi->Instance->DATA32;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < dataLength; i++)
+        {
+            spifi->Instance->DATA8 = dataBytes[i];
+        }
+    }
+}
