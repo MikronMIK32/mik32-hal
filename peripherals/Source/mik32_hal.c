128f555dfbc8d9720cbfb58653261a37377c9936
Barsy_Barsevich <116821312+Barsy-Barsevich@users.noreply.github.com>
2024-05-07 20:09:00 +0500

    System time functions added

--- peripherals/Source/mik32_hal.c
+++ peripherals/Source/mik32_hal.c
@@ -13,3 +13,77 @@ HAL_StatusTypeDef HAL_Init()
 
 	return HAL_OK;
 }
+
+
+/**
+ * @brief Функция программной задержки в миллисекундах
+ * @p time_ms - время в миллисекундах. Максимальное значение - 0xFFFFFFFF
+ * 
+ * TODO: добавить обработку случая, когда time_ms = 0
+ */
+__attribute__((section(".ram_text")))void HAL_ProgramDelayMs(uint32_t time_ms)
+{
+    uint32_t temp;
+    asm volatile(
+        "beq    %[count], x0, end_metka_%="         "\n\t"
+        "cycle_main_%=:"
+        //"lui    %[temp], 0"                         "\n\t"
+        //"addi   %[temp], %[temp], 1777"             "\n\t"
+        "li     %[temp], 3554"                      "\n\t"
+        "cycle_internal_%=:"
+        "addi   %[temp], %[temp], -1"               "\n\t"
+        "bne    %[temp], x0, cycle_internal_%="     "\n\t"
+        "addi   %[count], %[count], -1"             "\n\t"
+        "bne    %[count], x0, cycle_main_%="        "\n\t"
+        "end_metka_%=:"
+        ::
+        [count] "r" (time_ms),
+        [temp] "r" (temp)
+    );
+}
+
+/**
+ * @brief Функция программной задержки в микросекундах
+ * @p time_us - время в микросекундах. Максимальное значение - 0xFFFFFFFF
+ * 
+ * TODO: добавить обработку случая, когда time_us = 0
+ */
+__attribute__((section(".ram_text")))void HAL_ProgramDelayUs(uint32_t time_us)
+{
+    uint32_t temp;
+    time_us >>= 1;
+    asm volatile(
+        "beq    %[count], x0, end_metka_%="         "\n\t"
+        "cycle_main_%=:"
+        "li     %[temp], 7"                         "\n\t"
+        "cycle_internal_%=:"
+        "addi   %[temp], %[temp], -1"               "\n\t"
+        "bne    %[temp], x0, cycle_internal_%="     "\n\t"
+        "addi   %[count], %[count], -1"             "\n\t"
+        "bne    %[count], x0, cycle_main_%="        "\n\t"
+        "end_metka_%=:"
+        ::
+        [count] "r" (time_us),
+        [temp] "r" (temp)
+    );
+}
+
+__attribute__((weak)) uint32_t HAL_Micros()
+{
+    return 0;
+}
+
+__attribute__((weak)) uint32_t HAL_Millis()
+{
+    return 0;
+}
+
+__attribute__((weak)) void HAL_DelayMs(uint32_t time_ms)
+{
+    HAL_ProgramDelayMs(time_ms);
+}
+
+__attribute__((weak)) void HAL_DelayUs(uint32_t time_us)
+{
+    HAL_ProgramDelayUs(time_us);
+}
