6d792de3e8c2194b92783f2d21bf1a2fd69a2cae
Barsy_Barsevich <116821312+Barsy-Barsevich@users.noreply.github.com>
2024-05-14 19:30:03 +0500

    Update mik32_hal.c

--- peripherals/Source/mik32_hal.c
+++ peripherals/Source/mik32_hal.c
@@ -23,48 +23,38 @@ HAL_StatusTypeDef HAL_Init()
  */
 __attribute__((section(".ram_text")))void HAL_ProgramDelayMs(uint32_t time_ms)
 {
-    uint32_t temp;
     asm volatile(
         "beq    %[count], x0, end_metka_%="         "\n\t"
         "cycle_main_%=:"
-        //"lui    %[temp], 0"                         "\n\t"
-        //"addi   %[temp], %[temp], 1777"             "\n\t"
-        "li     %[temp], 3554"                      "\n\t"
+        //"lui    t0, 0"                              "\n\t"
+        //"addi   t0, %[temp], 1777"                  "\n\t"
+        "li     t0, 3554"                           "\n\t"
         "cycle_internal_%=:"
-        "addi   %[temp], %[temp], -1"               "\n\t"
-        "bne    %[temp], x0, cycle_internal_%="     "\n\t"
+        "addi   t0, t0, -1"                         "\n\t"
+        "bne    t0, x0, cycle_internal_%="          "\n\t"
         "addi   %[count], %[count], -1"             "\n\t"
         "bne    %[count], x0, cycle_main_%="        "\n\t"
         "end_metka_%=:"
         ::
-        [count] "r" (time_ms),
-        [temp] "r" (temp)
+        [count] "r" (time_ms)
     );
 }
 
-/**
- * @brief Функция программной задержки в микросекундах
- * @p time_us - время в микросекундах. Максимальное значение - 0xFFFFFFFF
- * 
- * TODO: добавить обработку случая, когда time_us = 0
- */
 __attribute__((section(".ram_text")))void HAL_ProgramDelayUs(uint32_t time_us)
 {
-    uint32_t temp;
     time_us >>= 1;
     asm volatile(
         "beq    %[count], x0, end_metka_%="         "\n\t"
         "cycle_main_%=:"
-        "li     %[temp], 7"                         "\n\t"
+        "li     t0, 7"                              "\n\t"
         "cycle_internal_%=:"
-        "addi   %[temp], %[temp], -1"               "\n\t"
-        "bne    %[temp], x0, cycle_internal_%="     "\n\t"
+        "addi   t0, t0, -1"                         "\n\t"
+        "bne    t0, x0, cycle_internal_%="          "\n\t"
         "addi   %[count], %[count], -1"             "\n\t"
         "bne    %[count], x0, cycle_main_%="        "\n\t"
         "end_metka_%=:"
         ::
-        [count] "r" (time_us),
-        [temp] "r" (temp)
+        [count] "r" (time_us)
     );
 }
 
